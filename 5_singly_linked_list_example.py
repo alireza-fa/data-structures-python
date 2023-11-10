b3f384121fb56a38acbe24be49819fe8f12e0524
alireza-fa <alirezafeyze44@gmail.com>
2023-11-10 04:42:03 +0330

    improving single linked list example

--- 5_singly_linked_list_example.py
+++ 5_singly_linked_list_example.py
@@ -6,6 +6,9 @@ class Node:
         self.data = data
         self.next = None
 
+    def __str__(self):
+        return f'data: {self.data}'
+
 
 class SinglyLinkedList:
 
@@ -14,11 +17,15 @@ class SinglyLinkedList:
         self.__tail = None
         self.__size = 0
 
-    def __set_new_head(self, node: Node) -> None:
-        self.__head = node
+    def size(self) -> int:
+        return self.__size
 
-    def __set_new_tail(self, node: Node) -> None:
-        self.__tail = node
+    def __update_size(self) -> None:
+        self.__size += 1
+
+    def insure_index(self, index: int) -> None:
+        if index > self.size() - 1:
+            raise IndexError
 
     def head(self) -> Node:
         return self.__head
@@ -26,31 +33,41 @@ class SinglyLinkedList:
     def tail(self) -> Node:
         return self.__tail
 
-    def __update_size(self) -> None:
-        self.__size += 1
+    def __set_head(self, node: Node) -> None:
+        self.__head = node
 
-    def size(self) -> int:
-        return self.__size
-
-    def insure_index(self, index: int) -> None:
-        try:
-            if index > self.size():
-                raise IndexError
-        except ValueError:
-            raise IndexError
+    def __set_tail(self, node: Node) -> None:
+        self.__tail = node
 
     def append(self, data: Any) -> None:
         new_node = Node(data=data)
+
         if self.head() is None:
-            self.__set_new_head(node=new_node)
+            self.__set_head(node=new_node)
 
         if self.tail() is None:
-            self.__set_new_tail(node=new_node)
+            self.__set_tail(node=new_node)
         else:
             self.tail().next = new_node
-            self.__set_new_tail(node=new_node)
+            self.__set_tail(node=new_node)
 
         self.__update_size()
+
+    def get_node(self, index: int) -> Node:
+        self.insure_index(index=index)
+
+        count = 0
+        current = self.head()
+
+        while index != count:
+            current = current.next
+            count += 1
+
+        return current
+
+    def get(self, index: int) -> Any:
+        node = self.get_node(index=index)
+        return node.data
 
     def set(self, index: int, data: Any) -> None:
         self.insure_index(index=index)
@@ -76,13 +93,13 @@ class SinglyLinkedList:
             current = current.next
             count += 1
 
-        previous.next = current.next
+        if previous:
+            previous.next = current.next
+        else:
+            self.__set_head(current.next)
         del current
 
-    def pop(self, index: Optional[int] = None) -> Any:
-        if index is None:
-            index = self.size() - 1
-
+    def pop(self, index: int = 0) -> Any:
         self.insure_index(index=index)
 
         count = 0
@@ -95,24 +112,24 @@ class SinglyLinkedList:
             count += 1
 
         if previous:
-            if current.next:
-                previous.next = current.next
-            else:
-                self.__set_new_tail(node=previous)
-                previous.next = None
+            previous.next = current.next
         else:
-            self.__set_new_head(node=current)
+            self.__set_head(node=current.next)
 
         data = current.data
         del current
-
         return data
 
-    def print_list(self) -> None:
+    def __str__(self) -> str:
+        my_list = []
+
         current = self.head()
+
         while current:
-            print(current.data)
+            my_list.append(current.data)
             current = current.next
+
+        return str(my_list)
 
 
 if __name__ == '__main__':
@@ -120,13 +137,9 @@ if __name__ == '__main__':
     singly_list.append(data='eggs')
     singly_list.append(data='bread')
     singly_list.append(data='cereal')
-    print(singly_list.size())
-    # singly_list.print_list()
-    # singly_list.set(index=1, data='boobs')
-    # singly_list.print_list()
-    print('--------------------------')
-    print(singly_list.pop())
-    print(singly_list.pop(index=0))
-    print('-----------------------')
-    # singly_list.remove(index=5)
-    singly_list.print_list()
+    singly_list.set(index=0, data='boobs')
+    # singly_list.remove(3)
+    singly_list.pop()
+    singly_list.pop()
+    print(singly_list)
+
